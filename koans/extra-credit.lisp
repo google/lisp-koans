1d6fa2fc32cdc95fd357081ddd3b72e2494471d9
Michał "phoe" Herda <phoe@disroot.org>
2020-05-08 21:19:01 +0200

    Update extra-credit

--- koans/extra-credit.lisp
+++ koans/extra-credit.lisp
@@ -1,8 +1,9 @@
-;; EXTRA CREDIT:
-;;
-;; Create a program that will play the Greed Game.
-;; Rules for the game are in GREED_RULES.TXT.
-;;
-;; You already have a DiceSet class and score function you can use.
-;; Write a player class and a Game class to complete the project.  This
-;; is a free form assignment, so approach it however you desire.
+;;; EXTRA CREDIT:
+;;;
+;;; Create a program that will play the Greed game.
+;;; The full rules for the game are in the file extra-credit.txt.
+;;;
+;;; You already have a DICE-SET class and a score function you can use.
+;;; Write a PLAYER class and a GAME class to complete the project.
+;;;
+;;; This is a free form assignment, so approach it however you desire.
