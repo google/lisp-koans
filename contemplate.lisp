921cc5c320347b07327c69b9a9a3b46cdce7a7a2
Michał "phoe" Herda <phoe@disroot.org>
2020-05-08 21:36:19 +0200

    Remove Google qualified domain name

--- contemplate.lisp
+++ contemplate.lisp
@@ -26,4 +26,4 @@
 
 #+quicklisp (ql:quickload :bordeaux-threads)
 
-(com.google.lisp-koans:main)
+(net.common-lisp.lisp-koans:main)
--- lisp-koans.lisp
+++ lisp-koans.lisp
@@ -12,12 +12,12 @@
 ;;; See the License for the specific language governing permissions and
 ;;; limitations under the License.
 
-(defpackage #:com.google.lisp-koans
+(defpackage #:net.common-lisp.lisp-koans
   (:use #:common-lisp
-        #:com.google.lisp-koans.test)
+        #:net.common-lisp.lisp-koans.test)
   (:export #:main))
 
-(in-package :com.google.lisp-koans)
+(in-package #:net.common-lisp.lisp-koans)
 
 (defvar *all-koan-groups*
   (with-open-file (in #p".koans")
@@ -28,7 +28,7 @@
 ;;; Functions for loading koans
 
 (defun package-name-from-group-name (group-name)
-  (format nil "COM.GOOGLE.LISP-KOANS.KOANS.~A" group-name))
+  (format nil "NET.COMMON-LISP.LISP-KOANS.KOANS.~A" group-name))
 
 (defun load-koan-group-named (koan-group-name)
   (let* ((koan-name (string-downcase (string koan-group-name)))
@@ -36,7 +36,7 @@
          (koan-package-name (package-name-from-group-name koan-group-name)))
     (unless (find-package koan-package-name)
       (make-package koan-package-name
-                    :use '(#:common-lisp #:com.google.lisp-koans.test)))
+                    :use '(#:common-lisp #:net.common-lisp.lisp-koans.test)))
     (let ((*package* (find-package koan-package-name)))
       (load (concatenate 'string "koans/" koan-file-name)))))
 
